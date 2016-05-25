e9755a5105644528a10f73521b87f65b917d1acd
amist <amistern0@gmail.com>
2016-05-25 19:01:29 +0300

    Add __init__.py

--- ge/core/genetic_executor.py
+++ ge/core/genetic_executor.py
@@ -5,8 +5,8 @@ class GeneticExecutor:
         
     def get_solution(self):
     
+        # TODO: replace with a real implementation
         class Sol():
             def get_fitness_value(self):
                 return 0
-                
         return Sol()
